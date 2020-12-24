e2412c572da24b898ea8095c1f2f8ca0dea6e123
RyouheiMatsuyama <ryohei.0427@ezweb.ne.jp>
2020-12-24 15:12:15 +0900

    Issue2 修正２

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -16,7 +16,7 @@ class CalendarsController < ApplicationController
     params.require(:calendars).permit(:date, :plan)
   end
 
-  def getweek
+  def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     @todays_date = Date.today
